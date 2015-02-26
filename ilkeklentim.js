0f36f47fb17cbbfc79ed57d3a402b57b91859b1a
vuralusenc <vuralusenc@gmail.com>
2015-02-26 21:52:26 +0200

    ilkeklentşm
    ilkeklentşm

---
+++ ilkeklentim.js
@@ -0,0 +1,14 @@
+﻿(function($){
+  $.fn.ilkeklentim = function(){
+    var elements = this.children("span");
+    $(elements)
+	.css("border","solid 1px #666")
+	.css("margin-bottom","2px")
+	.css("color","#000")
+	.css("padding","2px 5px")
+	.css("background-color","#eee")
+	.css("display","block")
+	.css("font-famiy","Arial")
+	.css("font-size","14px");
+  };
+})(jQuery);
